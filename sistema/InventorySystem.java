3b693f35a8d3e323b8484e6d59b997fc7526977f
Mikhael Canaza Vargas <mikhael.canaza@estudiante.ucsm.edu.pe>
2025-06-23 10:58:59 -0500

    Update InventorySystem.java

--- sistema/InventorySystem.java
+++ sistema/InventorySystem.java
@@ -25,7 +25,7 @@ public class InventorySystem {
      * Inicializa el sistema con estructuras vacías.
      * @throws ItemDuplicated 
      */
-    public InventorySystem() throws ItemDuplicated {
+    public InventorySystem() throws ItemDuplicated { //inicializamos las ESTRUC DATOS
         bPlusTree = new BPlusTree<>();
         graph     = new GraphLink<>();
         hashTable = new HashTable<>();
@@ -40,7 +40,7 @@ public class InventorySystem {
      * @throws ItemNotFound 
      * @throws IsEmpty 
      */
-    public void addItem(Item item) throws IsEmpty, ItemNotFound, ItemDuplicated {
+    public void addItem(Item item) throws IsEmpty, ItemNotFound, ItemDuplicated { //INSERTAMOS EN LOS 3 TIPOS DE ED
         // 1) Árbol B+
         bPlusTree.insert(item);
         // 2) HashTable
@@ -60,6 +60,7 @@ public class InventorySystem {
      * @throws ItemNotFound 
      * @throws IsEmpty 
      */
+    // CUANDO ELIMINAMOS TAMBIE MMANMOS A LAS 3 METODOS PARA EIMINARLOS CN RESPECTUVOS AE
     public boolean removeItem(String code) throws IsEmpty, ItemNotFound {
         Item it = hashTable.get(code);
         if (it == null) {
@@ -76,6 +77,7 @@ public class InventorySystem {
      * @throws ItemNotFound si no existe
      * @throws IsEmpty 
      */
+    //ENCONTRA UN IREM EN TABKAS HASH
     public Item getItem(String code) throws ItemNotFound, IsEmpty {
         Item it = hashTable.get(code);
         if (it == null) {
@@ -87,12 +89,11 @@ public class InventorySystem {
     /** Muestra por consola la estructura del B+ Tree. 
      * @throws ItemNotFound 
      * @throws IsEmpty */
-    public void displayTree() throws IsEmpty, ItemNotFound {
+    public void displayTree() throws IsEmpty, ItemNotFound {// AYUDA A IMPIMIR LA ESTRUCTURA JERRAQUICA
         bPlusTree.display();
     }
-
     /** Muestra por consola la representación del grafo de ubicaciones. */
-    public void displayGraph() {
+    public void displayGraph() {// MUESTRA LA ESTRUCTURA DEL GRAFO
         System.out.println(graph);
     }
 
@@ -101,8 +102,8 @@ public class InventorySystem {
      * @param edge  en formato "NodoA-NodoB"
      * @param enable true para crear la arista, false para eliminarla
      */
-    public void toggleEdge(String edge, boolean enable) {
-        String[] parts = edge.split("-");
+    public void toggleEdge(String edge, boolean enable) {//ACTIVA O DESACTVA LAS CONEXIOBES DE LA UBICAION EN EL ALMACEN
+        String[] parts = edge.split("-"); //ANALIZA LA CADEN EDGE
         if (parts.length != 2) return;
 
         try {
@@ -120,9 +121,9 @@ public class InventorySystem {
      * Simula un escenario de optimización de rutas entre "Entrada" y "Salida"
      * usando el método shortestPath de GraphLink.
      */
-    public void simulate() {
+    public void simulate() { // SIMULA UNA OPTIMIZACION DE RUTA DENTRO DEL ALAMACEN
         try {
-            LinkedList<String> path = graph.shortestPath("Entrada", "Salida");
+            LinkedList<String> path = graph.shortestPath("Entrada", "Salida");// ACTIA EL DISTRAC O EL BFS, DFS
             System.out.println("Ruta óptima: " + path);
         } catch (Exception e) {
             System.out.println("Error al simular ruta: " + e.getMessage());
